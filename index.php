6a18e3605169b0a1686e02c00bc7819fa0a077b1
Daniele Fusari <danielefusari@Danieles-iMac.local>
2021-11-30 11:34:53 +0000

    google

--- index.php
+++ index.php
@@ -23,13 +23,13 @@ ini_set('html_errors', 0);
 	<link href='//fonts.googleapis.com/css?family=Raleway:400,300,600,700' rel='stylesheet' type='text/css'>
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-RCH259RF50"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-213828274-1"></script>
 	<script>
 	  window.dataLayer = window.dataLayer || [];
 	  function gtag(){dataLayer.push(arguments);}
 	  gtag('js', new Date());
 
-	  gtag('config', 'G-RCH259RF50');
+	  gtag('config', 'UA-213828274-1');
 	</script>
 </head>
 
